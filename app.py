ce5c042b008a1ae4ccc0c07429ac22f97a238c61
Vijay Sandeep <vijaysandeep2002@gmail.com>
2026-04-09 00:19:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 st.set_page_config(page_title="AI Content Generator", page_icon="✨")
 st.title("✨ AI Content Generator")
